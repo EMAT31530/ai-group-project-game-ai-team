116207ea4529372768c999bef280c521a42ebdf7
jdRanda <randa.jtd@gmail.com>
2020-11-09 11:49:41 +0000

    developing  classes

--- AIclasses.py
+++ AIclasses.py
@@ -1,19 +1,30 @@
 import random as rnd
 
 
-class Card:
+class Card:  # Object to represent individual cards
     def __init__(self, suit, val):
         self.suit = suit
         self.val = val
+        self.visible = False  # If card is visible to other players
 
-    def getcard(self):
-        return '{} of {}'.format(self.val, self.suit)
+    def __str__(self):  # Overwrites the String fucntion
+        vals = [2, 3, 4, 5, 6, 7, 8, 9, 10, 'Jack', 'Queen', 'King', 'Ace']
+        return '{} of {}'.format(vals[self.val-1], self.suit)
+
+    def __lt__(self, other):  # Overwrites the less than function
+        return self.val < other.val
+
+    def __gt__(self, other):  # Overwrites the greater than function
+        return self.val > other.val
 
 
-class Deck:
+class Deck:  # Object to represent deck throughout game
     def __init__(self):
         self.cards = []
         self.build()
+
+    def __str__(self):  # Overwrites the String fucntion
+        return str([str(card) for card in self.cards])
 
     def build(self):
         for suit in ["Hearts", "Diamonds", "Spades", "Clubs"]:
@@ -29,10 +40,12 @@ class Deck:
         return self.cards.pop(-1)
 
 
+class Hand:  # Object to represent player hands
+    def __init__(self):
+        self.cards = []
+
+
 newdeck = Deck()
-for i in range(4):
-    print(newdeck.cards[i].getcard())
-newdeck.shuffle()
-for i in range(4):
-    print(i)
-    print(newdeck.cards[i].getcard())
+print(newdeck)
+newdeck.draw(burn=True)
+print(newdeck)
