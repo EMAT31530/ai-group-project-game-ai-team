7584c467d4dc59effe933cb2835531eb9c2bfeb4
Arnold <arniethepie@gmail.com>
2020-12-04 01:13:39 +0000

    Almost done concept

--- graphics test.py
+++ graphics test.py
@@ -1,4 +1,14 @@
 from graphics import *
+from AIclasses import *
+
+
+def playerinit(playernames, funds, graphwin):
+    # player names
+    Text(Point(75, 50), 'Player: %s' % playernames[0]).draw(graphwin).setSize(13)
+    Text(Point(75, 950), 'Player: %s' % playernames[1]).draw(graphwin).setSize(13)
+    # player funds
+    Text(Point(75, 25), 'Funds: $%s' % funds[0]).draw(graphwin).setSize(13)
+    Text(Point(75, 975), 'Funds: $%d' % funds[1]).draw(graphwin).setSize(13)
 
 
 def carddraw(num, suit, point, graphwin):
@@ -13,6 +23,37 @@ def carddraw(num, suit, point, graphwin):
     # suit
     Text(Point(point[0], point[1]), suit).draw(graphwin).setSize(18)
 
+
+def boardupdate(pot, currentbet, graphwin):
+    Text(Point(75, 525), 'Pot: $%d' % pot).draw(graphwin).setSize(13)
+    Text(Point(75, 475), 'Current bet: $%d' % currentbet).draw(graphwin).setSize(13)
+
+
+def player1hand(hand1, graphwin):
+    # player 1
+    carddraw(hand[0], hand[1], (445, 100), graphwin)
+
+    carddraw(hand[3], hand[4], (555, 100), graphwin)
+
+
+def player2hand(hand2, graphwin):
+    # player 2
+    carddraw(hand[0], hand[1], (445, 900), graphwin)
+
+    carddraw(hand[3], hand[4], (555, 900), graphwin)
+
+
+def board(hand3, graphwin):
+    # board
+    carddraw(10, '♠', (280, 500), win)
+
+    carddraw('J', '♦', (390, 500), win)
+
+    carddraw('Q', '♥', (500, 500), win)
+
+    carddraw('K', '♦', (610, 500), win)
+
+    carddraw('A', '♠', (720, 500), win)
 
 
 def main():
@@ -39,6 +80,16 @@ def main():
     carddraw('K', '♦', (610, 500), win)
 
     carddraw('A', '♠', (720, 500), win)
+
+    boardupdate(500, 5000, win)
+    playerinit(['Arnold', 'Joe'], [500, 600], win)
+
+
+
+
+
+
+
 
 
     win.setBackground('green')
